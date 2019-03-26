7044ddafc73a832c2520c5fbb2f5f9f35ee38aed
Eivind Bøhn <eivind.bohn@gmail.com>
2019-03-26 16:19:01 +0100

    remove atari install

--- setup.py
+++ setup.py
@@ -105,7 +105,7 @@ setup(name='stable_baselines',
       packages=[package for package in find_packages()
                 if package.startswith('stable_baselines')],
       install_requires=[
-          'gym[atari,classic_control]>=0.10.9',
+          'gym[classic_control]>=0.10.9',
           'scipy',
           'joblib',
           'mpi4py',
