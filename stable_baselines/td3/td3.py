fix bug with q_filter noise scaling

stable_baselines/td3/td3.py
@@ -619,7 +619,8 @@ class TD3(OffPolicyRLModel):
                 obs = np.concatenate([obs_dict["observation"], obs_dict["achieved_goal"]])
                 action_prev = np.zeros(shape=self.action_space.shape)
 
-            self.q_filter_moving_average.append(1)
+            if self.q_filter_scale_noise:
+                self.q_filter_moving_average.append(1)
 
             for step in range(initial_step, total_timesteps):
                 if callback is not None: