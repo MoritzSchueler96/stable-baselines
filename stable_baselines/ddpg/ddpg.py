18077accb24f668755a48874f2e234e2c6d1ef30
Eivind Bøhn <eivind.bohn@gmail.com>
2019-12-02 13:17:12 +0100

    add simple FixedWingAircraft support for DDPG

--- stable_baselines/ddpg/ddpg.py
+++ stable_baselines/ddpg/ddpg.py
@@ -511,6 +511,7 @@ class DDPG(OffPolicyRLModel):
             logger.info('setting up critic optimizer')
         normalized_critic_target_tf = tf.clip_by_value(normalize(self.critic_target, self.ret_rms),
                                                        self.return_range[0], self.return_range[1])
+        self.normalized_critic_target_tf = normalized_critic_target_tf
         self.critic_loss = tf.reduce_mean(tf.square(self.normalized_critic_tf - normalized_critic_target_tf))
         if self.critic_l2_reg > 0.:
             critic_reg_vars = [var for var in tf_util.get_trainable_vars('model/qf/')
@@ -825,10 +826,11 @@ class DDPG(OffPolicyRLModel):
                 logger.log('Using agent with the following configuration:')
                 logger.log(str(self.__dict__.items()))
 
+            ep_info_buf = deque(maxlen=100)
+
             eval_episode_rewards_history = deque(maxlen=100)
             episode_rewards_history = deque(maxlen=100)
             episode_successes = []
-
 
             with self.sess.as_default(), self.graph.as_default():
                 # Prepare everything.
@@ -898,6 +900,17 @@ class DDPG(OffPolicyRLModel):
                             if callback.on_step() is False:
                                 callback.on_training_end()
                                 return self
+
+                            # Retrieve reward and episode length if using Monitor wrapper
+                            maybe_ep_info = info.get('episode')
+                            if maybe_ep_info is not None:
+                                ep_info_buf.extend([maybe_ep_info])
+
+                            if writer is not None:
+                                ep_rew = np.array([reward]).reshape((1, -1))
+                                ep_done = np.array([done]).reshape((1, -1))
+                                self.episode_reward = tf_util.total_episode_reward_logger(self.episode_reward, ep_rew, ep_done,
+                                                                                  writer, self.num_timesteps)
 
                             step += 1
                             total_steps += 1
